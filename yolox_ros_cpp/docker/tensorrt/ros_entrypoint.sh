33ae5d4d5c1e2a089eac90b791a7c14324af9019
Yusuke Mizoguchi <sky.y.m.318@gmail.com>
2022-06-21 22:09:03 +0900

    save

--- yolox_ros_cpp/docker/tensorrt/ros_entrypoint.sh
+++ yolox_ros_cpp/docker/tensorrt/ros_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /opt/ros/foxy/setup.bash
-source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+source /opt/ros/noetic/setup.bash
+#source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
